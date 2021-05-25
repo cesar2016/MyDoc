1cd52467fa8dbec53b621801588ad7bb2003c948
cesar2016 <cesars.pro@gmail.com>
2021-05-25 10:06:12 -0300

    Complete db con faker

--- database/seeders/UsersTableSeeders.php
+++ database/seeders/UsersTableSeeders.php
@@ -1,19 +1,18 @@
 <?php
 
 namespace Database\Seeders;
-
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UsersTableSeeders extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {        
-        $this->call(UsersTableSeeders::class);         
-       
+        User::factory()->count(50)->create(); 
     }
 }
